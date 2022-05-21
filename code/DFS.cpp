fb3f00d0cf9fa7133e5559a4fcf38bafb5d53bed
SunsetGlow95 <87650221+SunsetGlow95@users.noreply.github.com>
2022-05-21 22:40:35 +0800

    Update DFS.cpp

--- code/DFS.cpp
+++ code/DFS.cpp
@@ -1,44 +1,35 @@
 /**
  * @file DFS.cpp
  * @author mzx
- * @version 0.1
- * @date 2022-02-25
+ * @version 0.2
+ * @date 2022-05-21
  */
-#include <stack>
-
 #include "Maze.h"
 using namespace maze;
 using namespace std;
 
-struct Stat {
-  int x, y, steps;
-  Stat(int _x = -1, int _y = -1, int _s = 0) : x(_x), y(_y), steps(_s) {}
-};
+bool dfs(int x, int y) {
+  if (x == HEIGHT - 2 && y == WIDTH - 2) return true;
+  graph[x][y] = CUR;
+  print();
+  for (int i(0); i != DIRs; ++i) {
+    int nx(x + Xs[i]), ny(y + Ys[i]);
+    if (graph[nx][ny] == UNVST) {
+      befx[nx][ny] = x;
+      befy[nx][ny] = y;
+      if (dfs(nx, ny)) {
+        return true;
+      }
+    }
+  }
+  graph[x][y] = VST;
+  print();
+  return false;
+}
 
 int main() {
   init();
-  stack<Stat> q;
-  q.push(Stat(1, 1));
-  graph[1][1] = CUR;
-  while (!q.empty()) {
-    Stat cur(q.top()), nxt;
-    q.pop();
-    if (cur.x == HEIGHT - 2 && cur.y == WIDTH - 2) {
-      break;
-    }
-    nxt.steps = cur.steps + 1;
-    for (int i(0); i != DIRs; ++i) {
-      nxt.x = cur.x + Xs[i], nxt.y = cur.y + Ys[i];
-      if (graph[nxt.x][nxt.y] == UNVST) {
-        graph[nxt.x][nxt.y] = CUR;
-        befx[nxt.x][nxt.y] = cur.x;
-        befy[nxt.x][nxt.y] = cur.y;
-        q.push(nxt);
-      }
-    }
-    graph[cur.x][cur.y] = VST;
-    print();
-  }
+  dfs(1, 1);
   goback();
   return 0;
 }
